8bdd8682934c7e64085bfecd7a0981b163f35f62
silemi5 <sileofthefive@gmail.com>
2017-01-15 07:57:42 +0800

    Stated working on FCFS

---
+++ algo/fcfs.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST['process']) && isset($_POST['algo']))
+    {
+        $processes = $_POST['process'];
+        $algo = $_POST['algo'];
+    }
+    else
+        exit("Error!");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
