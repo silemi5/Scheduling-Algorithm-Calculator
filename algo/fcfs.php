1805d39904d5474ecfc14315e70320e7d15d073f
silemi5 <sileofthefive@gmail.com>
2017-01-15 10:22:01 +0800

    FCFS Done

--- algo/fcfs.php
+++ algo/fcfs.php
@@ -8,6 +8,45 @@
     }
     else
         exit("Error!");
+    
+    //populates the inital queue
+    for($x = 0; $x < $NumProcess; $x++)
+        $queue[] = $x;
+        $CT[] = 0;
+
+    //rearranges the queue
+    for($x = 1; $x < $NumProcess; $x++)
+    {
+        for ($y = 1; $y < $NumProcess; $y++)
+        {
+            if ($AT[$queue[$y]] < $AT[$queue[$y - 1]])
+            {
+                $temp = $queue[$y];
+                $queue[$y] = $queue[$y - 1];
+                $queue[$y - 1] = $temp;
+            }
+        }
+    }
+
+//calculates the CT
+    $time = 0;
+    for($x = 0; $x < $NumProcess; $x++)
+    {
+        $time += $BT[$queue[$x]];
+        $CT[$queue[$x]] = $time;
+    }
+//calculates the TT, WT
+    for($x = 0; $x < $NumProcess; $x++)
+    {
+        $TT[] = $CT[$x] - $AT[$x];
+    }
+
+    for($x = 0; $x < $NumProcess; $x++)
+    {
+        $WT[] = $TT[$x] - $BT[$x];
+    }
+    
+            
 ?>
 
 <!DOCTYPE html>
@@ -41,9 +80,9 @@
                                 <td>" . $AT[$x] . "</td>
                                 <td>" . $BT[$x] . "</td>
                                 <td>" . $Prio[$x] . "</td>
-                                <td>CT1</td>
-                                <td>TT</td>
-                                <td>WT</td>
+                                <td>" . $CT[$x] . "</td>
+                                <td>" . $TT[$x] . "</td>
+                                <td>" . $WT[$x] . "</td>
                             </tr>
                         ");
                     }
